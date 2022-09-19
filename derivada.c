e2597cf32255d67880c20c218de735b0754a69d9
rfrazera7 <93052422+rfrazera7@users.noreply.github.com>
2022-09-19 15:30:08 -0300

    Add files via upload

---
+++ derivada.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+///PROF, este programa deriva apenas uma vez, e a cada derivação é necessário mudar o nome do arquivo (linhas 15 e 27 do código)
+
+main()
+{
+int t, a, ch, N, i;
+double I=0;
+float dx=0, x, df;
+
+FILE *arq;
+
+
+arq = fopen ("ida1.dat", "r");
+
+do //PARA CONTAR NUMERO DE LINHAS DO ARQUIVO
+        {
+            ch = fgetc(arq);
+            if(ch == '\n')
+            N++;
+        }
+while(ch != EOF);
+
+fclose (arq);
+
+arq = fopen ("ida1.dat", "r"); //ARQUIVO COM DADOS QUE VOU DERIVAR (PARA DERIVAR UMA SEGUNDA VEZ MUDOU-SE ESTE NOME)
+
+
+float ponto[N][2];
+
+/*fscanf (arq, "%f", &ponto[i][0]);
+fscanf (arq, "%f", &ponto[i][1]);*/
+
+for (i = 0; i < N; i++) //LEITURA DOS DADOS DO ARQUIVO
+{
+	fscanf (arq, "%f", &ponto[i][0]);
+	fscanf (arq, "%f", &ponto[i][1]);
+}
+for(i = 1; i < N; i++)
+{
+	dx=ponto[i][0]-ponto[i-1][0];
+	df=ponto[i][1]-ponto[i-1][1];
+	printf("%f\t%f\n", ponto[i][0], df/dx);
+}
+fclose (arq);
+}
---
+++ integra2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+
+///PROF, este programa integra apenas uma vez, e a cada integração é necessário mudar o nome do arquivo (linhas 15 e 27 do código)
+
+main()
+{
+int t, a, ch, N, i;
+double I=0;
+float dx=0.001, x;
+
+FILE *arq;
+//FILE *arq2;
+
+arq = fopen ("idaevolta.dat", "r");
+
+do //CONTA NUMERO DE LINHAS
+        {
+            ch = fgetc(arq);
+            if(ch == '\n')
+            N++;
+        }
+while(ch != EOF);
+
+fclose (arq);
+
+arq = fopen ("idaevolta.dat", "r"); // a cada integração, vinha-se neste arquivo e muda-se o nome do arquivo a ser integrado
+//arq2 = fopen ("reta2.dat", "w");
+
+float ponto[N][2];
+
+for (i = 0; i < N; i++)
+{
+	fscanf (arq, "%f", &ponto[i][0]);
+	fscanf (arq, "%f", &ponto[i][1]);
+	ponto[i][1]=ponto[i][1]*9.8;
+}
+
+/*for (i = 0; i < N; i++)
+{
+	ponto[i][1]=-ponto[i][1]-1;
+}*/
+
+for(i = 0; i < N-1; i++)
+    {
+        dx = ponto[i+1][0]-ponto[i][0];
+        I += dx*ponto[i][1];
+        printf("%f\t%f\n", ponto[i][0], I+ponto[0][1]);
+    }
+
+}
